417438e7abf368f88de6bbebef63cae9cf129fe7
mddalam1 <mddalam1@gmail.com>
2020-12-16 18:13:27 +0530

    Made More Effective Sort FUnctionality.

--- src/components/TableHeader/TablleHeader.component.jsx
+++ src/components/TableHeader/TablleHeader.component.jsx
@@ -10,13 +10,23 @@ class TableHeader extends Component {
         sortColumn = { path, order : 'asc'}
         this.props.handleSort(sortColumn)
     }
+    renderSortIcon = column => {
+        if(column.path !== this.props.sortColumn.path) return null
+        if(this.props.sortColumn.order === 'asc') return <i className="fa fa-sort-asc" aria-hidden="true"></i>
+        return <i class="fa fa-sort-desc" aria-hidden="true"></i>
+
+    }
 
     render() { 
         const {columns} = this.props
         return ( 
             <thead>
                 <tr>
-                    {columns.map(column => <th key={column.path || column.key} onClick={()=> {this.raiseSort(column.path)}}>{column.label}</th>)}
+                    {columns.map(column => 
+                        <th style={{cursor:'pointer'}}
+                            key={column.path || column.key} 
+                            onClick={()=> {this.raiseSort(column.path)}}
+                        >{column.label} {this.renderSortIcon(column)}</th>)}
                 </tr>
             </thead>
          )
