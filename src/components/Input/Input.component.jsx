9756af51125c0fbe5bc67f84ead7ff5dbd4f73ab
mddalam1 <mddalam1@gmail.com>
2020-12-18 18:07:13 +0530

    validated Form with Custom validations.

--- src/components/Input/Input.component.jsx
+++ src/components/Input/Input.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Input = ({handleChange, label, name, ...otherProps}) => {
+const Input = ({handleChange, label, name, error, ...otherProps}) => {
     return (
         <div className="form-group">
             <label htmlFor={name}>{label}</label>
             <input type="text" id={name} name={name} {...otherProps} className="form-control" onChange={handleChange}/>
+            {error && <div className="alert alert-danger small py-2 mt-1"> <i className="fa fa-exclamation-triangle" aria-hidden="true"></i> {error}</div>} 
         </div>
      );
 }
--- src/components/LoginForm/LoginForm.component.jsx
+++ src/components/LoginForm/LoginForm.component.jsx
@@ -7,28 +7,55 @@ class LoginForm extends Component {
         account : {
             username : '', 
             password : ''
-        } 
+        },
+        errors : {} 
+    }
+    validate = () => {
+        const {username, password} = this.state.account
+        const errors = {}
+        if(username.trim() === '') 
+        errors.username = 'Username is required.'
+        if(password.trim() === '') 
+        errors.password = 'Password is required.'
+        return !Object.keys(errors).length ? null : errors
+    }
+    validateProperty = ({name, value}) => {
+        if(name === 'username') {
+            if(value.trim() === '') return 'Username is required.'
+        }
+        if(name === 'password') {
+            if(value.trim() === '') return 'Password is required.'
+        }
     }
     handleChange = (e) => {
         const {name, value} = e.target
+        const errors = {...this.state.errors}
+        const errorMessage = this.validateProperty(e.target)
+        if(errorMessage) errors[name] = errorMessage
+        else delete errors[name]
         const account = {...this.state.account}
         account[name] = value
-        this.setState({account})
+        this.setState({account, errors})
     }
     handleSubmit = (e) => {
         e.preventDefault()
+        const errors = this.validate()
+        this.setState({errors : errors || {} })
+        if(errors) return
+
         this.setState({account : {username : '', password : '' }})
         console.log('Submitted!!!!')
     }
 
     render() { 
         const {username, password} = this.state.account
+        const {errors} = this.state
         return ( 
             <div>
                 <h1>Login</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <Input autoFocus type='text' value={username} name='username' label='Username' handleChange={this.handleChange} />
-                    <Input type='password' value={password} name='password' label='Password' handleChange={this.handleChange} />
+                    <Input autoFocus type='text' error={errors.username} value={username} name='username' label='Username' handleChange={this.handleChange} />
+                    <Input type='password' error={errors.password} value={password} name='password' label='Password' handleChange={this.handleChange} />
                     <Button className='btn btn-primary btn-lg'>Login</Button>
                 </form>
             </div>
