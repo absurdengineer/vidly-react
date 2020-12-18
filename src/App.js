74c41b9e621fa90429d7f21485d1c4d4bd155e42
mddalam1 <mddalam1@gmail.com>
2020-12-18 14:40:32 +0530

    Done with Navigation and Navigation Bar.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from 'react'
-import { Redirect, Route, Switch } from 'react-router-dom';
-import Movies from './components/Movies/Movies.component';
-import NotFound from './components/NotFound/NotFound.component';
-import Customer from './pages/Customer/Customer.page';
-import Rental from './pages/Rental/Rental.page';
+import { Redirect, Route, Switch } from 'react-router-dom'
+import Movies from './components/Movies/Movies.component'
+import NavBar from './components/NavBar/NavBar.component'
+import NotFound from './components/NotFound/NotFound.component'
+import Customer from './pages/Customer/Customer.page'
+import Rental from './pages/Rental/Rental.page'
+import './App.css'
 
 const App = () => {
   return ( 
-    <div className="container p-5">
-      <Switch>
-        <Route path='/movies' component={Movies} />
-        <Route path='/customers' component={Customer} />
-        <Route path='/rentals' component={Rental} />
-        <Route path='/not-found' component={NotFound} />
-        <Redirect from='/' exact to='/movies' />
-        <Redirect to='/not-found' />
-      </Switch>
-    </div>
+    <React.Fragment >
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path='/movies' component={Movies} />
+          <Route path='/customers' component={Customer} />
+          <Route path='/rentals' component={Rental} />
+          <Route path='/not-found' component={NotFound} />
+          <Redirect from='/' exact to='/movies' />
+          <Redirect to='/not-found' />
+        </Switch>
+      </main>
+    </React.Fragment>
    );
 }
 
