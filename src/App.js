86f6f0a824bfa397873c569000c7446b3bb3b5ad
mddalam1 <mddalam1@gmail.com>
2020-12-18 14:20:17 +0530

    Added Some Basic Routes Stuff.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom';
 import Movies from './components/Movies/Movies.component';
+import NotFound from './components/NotFound/NotFound.component';
+import Customer from './pages/Customer/Customer.page';
+import Rental from './pages/Rental/Rental.page';
 
 const App = () => {
   return ( 
     <div className="container p-5">
-      <Movies />
+      <Switch>
+        <Route path='/customers' component={Customer} />
+        <Route path='/rentals' component={Rental} />
+        <Route path='/not-found' component={NotFound} />
+        <Route path='/' component={Movies} />
+      </Switch>
     </div>
    );
 }
---
+++ src/pages/Rental/Rental.page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Rental = (props) => {
+    return ( 
+        <h1>Rental</h1>
+    );
+}
+
+export default Rental;
